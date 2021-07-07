3aa9866a39a03c2859e571aad94c59f7bbf340dd
jurijs.cudnovskis <jurijs.cudnovskis@finabay.com>
2021-07-07 20:53:36 +0300

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t airspace-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t copper-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
